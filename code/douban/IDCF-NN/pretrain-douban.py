d059af397c3ad9db472b1bbc6cef44c85065eb2c
abdurahman02 <63095991+abdurahman02@users.noreply.github.com>
2022-06-16 04:49:20 +0500

    Update pretrain-douban.py

--- code/douban/IDCF-NN/pretrain-douban.py
+++ code/douban/IDCF-NN/pretrain-douban.py
@@ -128,8 +128,9 @@ for epoch in range(n_epochs):
 
 	if RMSE < bestRMSE:
 		bestRMSE = RMSE
-		path='./pretrain-douban/'
-		print("Does path exist: " + str(os.path.exist("./pretrain-douban/"))
+# 		path='./pretrain-douban/'
+# 		print("Does path exist: " + str(os.path.exist("./pretrain-douban/"))
+# 		save_model(model, path='./pretrain-douban/')
 # 		save_model(model, )
 		      
 		      
